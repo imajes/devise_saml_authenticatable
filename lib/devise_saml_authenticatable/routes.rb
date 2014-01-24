d4f0bbb3c8bcab3e0ccdbac8763078478d90d380
James Cox <james@imaj.es>
2014-01-24 11:47:27 -0500

    Switch from match to post

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -4,7 +4,7 @@ ActionDispatch::Routing::Mapper.class_eval do
     resource :session, :only => [], :controller => controllers[:saml_sessions], :path => "" do
       get :new, :path => "saml/sign_in", :as => "new"
       post :create, :path=>"saml/auth"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
+      post :destroy, :path => mapping.path_names[:sign_out], :as => "destroy"
       get :metadata, :path=>"saml/metadata"
     end
   end
